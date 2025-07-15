f33308d5c96c1ad0e5682f49d4cde4f1b8e58ffb
uma maheswari <chittudiuma@gmail.com>
2025-07-15 16:14:27 +0530

    working on backend

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,25 @@
 //   console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
 // );
 // index.js
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+// const authRoutes = require('./routes/auth');
+
+// dotenv.config();
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api', authRoutes);
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`http://localhost:${process.env.PORT}`);
+//   app.get('/', (req, res) => {
+//   res.send('Backend API is working!');
+// });
+
+// });
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -26,12 +45,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', authRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`http://localhost:${process.env.PORT}`);
-  app.get('/', (req, res) => {
-  res.send('Backend API is working!');
+
+app.get('/', (req, res) => {
+  res.send('✅ Backend API is working!');
 });
 
+app.use('/api', authRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
